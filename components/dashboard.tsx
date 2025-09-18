5416fb5987410f0c4260a25387fe2685f80cdf97
devpima <ibrahimpima76@gmail.com>
2025-09-18 00:07:59 +0000

    build fix 102

--- components/dashboard.tsx
+++ components/dashboard.tsx
@@ -56,7 +56,7 @@ export function Dashboard() {
         </div>
 
         {/* Main Content - Enhanced Inheritance Form */}
-        <EnhancedInheritanceForm />
+        <InheritanceForm />
       </div>
     </div>
   );
